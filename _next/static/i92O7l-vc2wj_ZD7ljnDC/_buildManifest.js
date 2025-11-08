8594e8b54b2fc9b8615e28eb310bc1e2af5004fd
Mindaugas <m.dangveckis@gmail.com>
2025-11-08 13:22:44 +0200

    init

--- _next/static/i92O7l-vc2wj_ZD7ljnDC/_buildManifest.js
+++ _next/static/i92O7l-vc2wj_ZD7ljnDC/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/petstep-lt//_next/:path+",
+        "destination": "/petstep-lt/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
